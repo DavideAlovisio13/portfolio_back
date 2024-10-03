0ed731e9945c1639541e3878b958d7a88fba5bea
DavideAlovisio13 <alovisiod@gmail.com>
2024-10-03 15:53:13 +0200

    setup home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.app')
 @section('content')
-    <section class="container">
-        <h1>Section title</h1>
-        <p>section content</p>
+    <section class="container d-flex" id="home">
+        <div class="left w-50 d-flex align-items-center justify-content-center">
+            <div>
+                <h1 class="title">Davide Alovisio
+                </h1>
+            </div>
+        </div>
+        <div class="right d-flex align-items-center justify-content-center w-50">
+            <div class="parent">
+                <div class="card">
+                    <div class="logo">
+                        <span class="circle circle1"></span>
+                        <span class="circle circle2"></span>
+                        <span class="circle circle3"></span>
+                        <span class="circle circle4"></span>
+                        <span class="circle circle5">
+                        </span>
+                    </div>
+                    <div class="glass"></div>
+                    <div class="content">
+                        <img src="/images/logo_back.png" alt="logo" style="width: 400px; height: 400px">
+                    </div>
+                    <div class="bottom">
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
