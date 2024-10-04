25598279d13d9e62f080f066192b36bd8c951bc8
DavideAlovisio13 <alovisiod@gmail.com>
2024-10-04 11:28:14 +0200

    insert slug generate function

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'slug',
+    ];
+
+    public static function generateSlug($title)
+    {
+        $slug = Str::slug($title, '-');
+        $count = 1;
+        while (Project::where('slug', $slug)->first()) {
+            $slug = Str::of($title)->slug('-') . "-{$count}";
+            $count++;
+        }
+        return $slug;
+    }
 }
