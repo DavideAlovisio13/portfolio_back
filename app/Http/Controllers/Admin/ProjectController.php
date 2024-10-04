23f067b902a2188dffa70b9bfa9762f27af0e328
DavideAlovisio13 <alovisiod@gmail.com>
2024-10-04 14:54:33 +0200

    add store function

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreProjectRequest;
 
 class ProjectController extends Controller
 {
@@ -22,15 +23,17 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request, Project $project)
     {
-        //
+        $data = $request->validated();
+        $project->create($data);
+        return redirect()->route('admin.projects.index')->with('message', 'Project created successfully!');
     }
 
     /**
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.dash')
 
 @section('content')
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa doloribus nemo. Nemo iure sapiente id veniam praesentium accusamus eos, cum ipsum reiciendis, inventore cupiditate adipisci ullam aliquid est placeat.
+    <h2>All the Projects</h2>
+    @foreach ($projects as $project)
+        <ul>
+            <li>
+                {{ $project->title }}
+            </li>
+        </ul>
+    @endforeach
 @endsection
